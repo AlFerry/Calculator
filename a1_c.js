4d68e4702e883d06a0b7b3771ddacc2d1e58e774
AlFerry <92052899+AlFerry@users.noreply.github.com>
2021-11-13 00:20:08 -0600

    Add files via upload

---
+++ a1_c.js
@@ -0,0 +1,53 @@
+//Alyssa Ferry//
+
+function calcu(calcValue) {
+switch (calcValue) {
+    case 0:
+	  calc.output.value += '0';
+        break;
+    case 1:
+	  calc.output.value += '1';
+        break;
+   
+    case 2:
+	  calc.output.value += '2';
+        break;
+    case 3:
+	  calc.output.value += '3';
+        break;
+    case 4:
+	  calc.output.value += '4';
+        break;
+    case 5:
+	  calc.output.value += '5';
+        break;
+    case 6:
+	  calc.output.value += '6';
+        break;
+    case 7:
+	  calc.output.value += '7';
+        break;
+    case 8:
+	  calc.output.value += '8';
+        break;
+    case 9:
+	  calc.output.value += '9';
+        break;
+    case '+':
+	  calc.output.value += '+';
+        break;
+    case '-':
+	  calc.output.value += '-';
+        break;
+    case '*':
+	  calc.output.value += '*';
+        break;
+    case '/':
+	  calc.output.value += '/';
+        break;
+    case 'ce':
+	  calc.output.value = '';
+        break;
+    default: //no condition on this last else
+	  calc.output.value =  eval(calc.output.value);// <-- the eval() function turns the collection of string into a working math function
+}}
